dbabb6f269697a57de9f0668fbf2675996d0b004
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2016-02-12 10:41:49 +0000

    New version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ test_requirements = [
 
 setup(
     name='sd-python-wrapper',
-    version='0.1.19',
+    version='0.1.20',
     description="A python wrapper for the Server Density Api",
     long_description=readme + '\n\n', # + history,
     author="Jonathan Sundqvist",

e80c33a95654c34d5dac89bed994604aa6500fb9
NassimHC <NassimHC@users.noreply.github.com>
2018-02-13 01:58:33 +0000

    UP updates for /metrics/v3/
    Untested. There will be issues.

--- serverdensity/wrapper/metrics.py
+++ serverdensity/wrapper/metrics.py
@@ -7,23 +7,23 @@ from serverdensity import Response
 class Metrics(JsonObject):
 
     PATHS = {
-        'available': '/metrics/definitions/{}',
-        'get': '/metrics/graphs/{}'
+        'available': '/metrics/v3/metrics/{}',
+        'get': '/metrics/v3/query/{}'
     }
 
     def _validation(self, value):
         """Not needed"""
         pass
 
-    def available(self, _id, start, end, **kwargs):
+    def available(self, start, end, **kwargs):
         kwargs.setdefault('params', {})['start'] = start.isoformat()
         kwargs['params']['end'] = end.isoformat()
-        result = self.api.get(url=self.PATHS['available'].format(_id), **kwargs)
+        result = self.api.get(url=self.PATHS['available'], **kwargs)
         return [Response(item) for item in result]
 
-    def get(self, _id, start, end, filtering, **kwargs):
+    def get(self, start, end, filtering, **kwargs):
         kwargs.setdefault('params', {})['start'] = start.isoformat()
         kwargs['params']['end'] = end.isoformat()
         kwargs['params']['filter'] = json.dumps(filtering)
-        result = self.api.get(url=self.PATHS['get'].format(_id), **kwargs)
+        result = self.api.get(url=self.PATHS['get'], **kwargs)
         return [Response(item) for item in result]
